3f4c0233ea4a715bc3437c5edc359fa9cae9114d
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-11-28 12:48:22 -0500

    Created ConnectFour and Player class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,1 +1,19 @@
 # frozen_string_literal: true
+
+# Represents a game of connect four
+class ConnectFour
+  def initialize(player1, player2)
+    @players = [player1, player2]
+    @current_player = player1
+  end
+end
+
+# Models player attributes for Connect Four game
+class Player
+  attr_reader :marker, :name
+
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
+  end
+end
