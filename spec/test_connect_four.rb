3f44da21e93775d873f4c4d3ab4b11baf7ec902c
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-11-29 13:17:44 -0500

    Created select spot tests

--- spec/test_connect_four.rb
+++ spec/test_connect_four.rb
@@ -3,6 +3,38 @@
 require './lib/connect_four'
 
 describe ConnectFour do
+  describe '#select_spot' do
+    let(:player1) { instance_double(Player, name: 'player1', marker: 'X') }
+    let(:player2) { instance_double(Player, name: 'player2', marker: 'O') }
+    subject(:connect_four) { described_class.new(player1, player2) }
+
+    context 'when player 1 selects a spot' do
+      before do
+        allow(connect_four).to receive(:gets).and_return('3') # player input
+      end
+
+      it "updates the spot to player 1's marker" do
+        connect_four.send(:select_spot)
+        board = connect_four.instance_variable_get(:@board)
+        expect(board[3]).to eq(player1.marker)
+      end
+    end
+
+    context 'when player 2 selects a spot' do
+      before do
+        players = connect_four.instance_variable_get(:@players)
+        connect_four.instance_variable_set(:@current_player, players[1])
+        allow(connect_four).to receive(:gets).and_return('3') # player input
+      end
+
+      it "updates the spot to player 2's marker" do
+        connect_four.send(:select_spot)
+        board = connect_four.instance_variable_get(:@board)
+        expect(board[3]).to eq(player2.marker)
+      end
+    end
+  end
+
   describe '#switch_player' do
     let(:player1) { double('player1') }
     let(:player2) { double('player2') }
