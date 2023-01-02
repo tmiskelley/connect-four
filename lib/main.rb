31d759de72c3b8569256b1b874e676210addf4bb
Timothy Miskelley <miskelleytimothy@gmail.com>
2023-01-02 15:48:21 -0500

    Created main game functionality

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,8 @@
 # frozen_string_literal: true
+
+require './lib/connect_four'
+
+player1 = Player.new('Player 1', 'X')
+player2 = Player.new('Player 2', 'O')
+
+ConnectFour.new(player1, player2).play
